63a24dd012d38c2a11455a645afa8b075ad06fe8
orange4glace <orange4glace@gmail.com>
2017-09-30 03:47:17 +0900

    Changed - L_DEBUG is no longer wrapped by _DEBUG

--- NaN2/src/module/team_module/team_module.cpp
+++ NaN2/src/module/team_module/team_module.cpp
@@ -13,9 +13,7 @@ TeamModule::TeamModule() : Module<TeamModule>() {
 
 void TeamModule::Initialize(const void* args ...) {
   int team_num = *(int*)args;
-#ifdef _DEBUG
   L_DEBUG << "Team Module initialized with # of teams = " << team_num;
-#endif
   for (int i = 0; i < team_num; i ++)
     teams_.emplace_back(Team(i));
 }
@@ -31,9 +29,7 @@ void TeamModule::AddPlayer(Team* const team, Player* const player) {
     throw std::string("[TeamModule] player_team_map duplicated player. " + player->id());
 #endif
   player_team_map_.insert({ player, team });
-#ifdef _DEBUG
   L_DEBUG << "[TeamModule] Player added, Team = " << team->id() << ", Player = " << player->id();
-#endif
 }
 
 void TeamModule::RemovePlayer(Team* const team, Player* const player) {
@@ -43,9 +39,7 @@ void TeamModule::RemovePlayer(Team* const team, Player* const player) {
     throw std::string("[TeamModule] player_team_map no player. " + player->id());
 #endif
   player_team_map_.erase(player);
-#ifdef _DEBUG
   L_DEBUG << "[TeamModule] Player removed, Team = " << team->id() << ", Player = " << player->id();
-#endif
 }
 
 Team* const TeamModule::GetTeam(TeamID id) {
